15c02eec912cc9b8a391f9fa08ddd527025f98a7
slckzbn <ozbenselcuk@gmail.com>
2019-05-02 16:45:15 +0300

    add form app

---
+++ AracKiralamaFormApplication/AracKiralamaFormApplication/frmAracKiralama.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace AracKiralamaFormApplication
+{
+    partial class frmAracKiralama
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.isteklerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.araçlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kiralanabilirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kiralanmışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.araçKiralamaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.isteklerToolStripMenuItem,
+            this.araçlarToolStripMenuItem,
+            this.araçKiralamaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(818, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // isteklerToolStripMenuItem
+            // 
+            this.isteklerToolStripMenuItem.Name = "isteklerToolStripMenuItem";
+            this.isteklerToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.isteklerToolStripMenuItem.Text = "İstekler";
+            this.isteklerToolStripMenuItem.Click += new System.EventHandler(this.isteklerToolStripMenuItem_Click);
+            // 
+            // araçlarToolStripMenuItem
+            // 
+            this.araçlarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kiralanabilirToolStripMenuItem,
+            this.kiralanmışToolStripMenuItem});
+            this.araçlarToolStripMenuItem.Name = "araçlarToolStripMenuItem";
+            this.araçlarToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.araçlarToolStripMenuItem.Text = "Araçlar";
+            // 
+            // kiralanabilirToolStripMenuItem
+            // 
+            this.kiralanabilirToolStripMenuItem.Name = "kiralanabilirToolStripMenuItem";
+            this.kiralanabilirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kiralanabilirToolStripMenuItem.Text = "Kiralanabilir";
+            this.kiralanabilirToolStripMenuItem.Click += new System.EventHandler(this.kiralanabilirToolStripMenuItem_Click);
+            // 
+            // kiralanmışToolStripMenuItem
+            // 
+            this.kiralanmışToolStripMenuItem.Name = "kiralanmışToolStripMenuItem";
+            this.kiralanmışToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kiralanmışToolStripMenuItem.Text = "Kiralanmış";
+            this.kiralanmışToolStripMenuItem.Click += new System.EventHandler(this.kiralanmışToolStripMenuItem_Click);
+            // 
+            // araçKiralamaToolStripMenuItem
+            // 
+            this.araçKiralamaToolStripMenuItem.Name = "araçKiralamaToolStripMenuItem";
+            this.araçKiralamaToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.araçKiralamaToolStripMenuItem.Text = "Araç Kiralama";
+            this.araçKiralamaToolStripMenuItem.Click += new System.EventHandler(this.araçKiralamaToolStripMenuItem_Click);
+            // 
+            // frmAracKiralama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 424);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmAracKiralama";
+            this.Text = "Araç Kiralama";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmAracKiralama_FormClosed);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem isteklerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem araçlarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kiralanabilirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kiralanmışToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem araçKiralamaToolStripMenuItem;
+    }
+}
+
---
+++ AracKiralamaFormApplication/AracKiralamaFormApplication/frmGirisYap.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace AracKiralamaFormApplication
+{
+    partial class frmGirisYap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Location = new System.Drawing.Point(362, 252);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(75, 23);
+            this.btnGiris.TabIndex = 2;
+            this.btnGiris.Text = "Giriş Yap";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(306, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Şifre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(306, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Kullanıcı Adı";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(395, 201);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(100, 20);
+            this.txtSifre.TabIndex = 1;
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(395, 175);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtKullaniciAdi.TabIndex = 0;
+            // 
+            // frmGirisYap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Name = "frmGirisYap";
+            this.Text = "Giriş Yap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+    }
+}
