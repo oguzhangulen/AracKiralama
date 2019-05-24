9fa68090eb42f6b56e6ea9e1f3743ba28fd6df8a
Oguzhan gulen <ogzhangulen@gmail.com>
2019-05-24 15:04:37 +0300

    update console

--- console/console/Program.cs
+++ console/console/Program.cs
@@ -17,17 +17,13 @@ namespace console
         {
             RunAsync().GetAwaiter().GetResult();
         }
-        static async Task<Uri> CreateCarAsync(Car car)
+        
+        static async Task<RentedCar> UpdateRentedCarAsync(RentedCar car)
         {
-            HttpResponseMessage response = await client.PostAsJsonAsync("api/Cars", car);
+            HttpResponseMessage response = await client.PutAsJsonAsync("api/Rentacar", car);
             response.EnsureSuccessStatusCode();
-            return response.Headers.Location;
-        }
-        static async Task<Uri> CreateRentedCarAsync(RentedCar car)
-        {
-            HttpResponseMessage response = await client.PostAsJsonAsync("api/Rentacar", car);
-            response.EnsureSuccessStatusCode();
-            return response.Headers.Location;
+            car = await response.Content.ReadAsAsync<RentedCar>();
+            return car;
         }
         static async Task RunAsync()
         {
@@ -36,55 +32,49 @@ namespace console
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             try
             {
-                string i = "";
-                Console.WriteLine("Yeni araç eklemek için (1)\nAraç kiralamak için (2)\n");
-                i=Console.ReadLine();
-                if(i=="1")
-                {
-                    Car car = new Car();
-                    Console.Write("Airbag:");
-                    car.Airbag =Convert.ToBoolean(Console.ReadLine());
-                    Console.WriteLine("BaggageCapacity:");
-                    car.BaggageCapacity = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("CarModel:");
-                    car.CarModel = Console.ReadLine();
-                    Console.WriteLine("CarName:");
-                    car.CarName=Console.ReadLine();
-                    Console.WriteLine("CompanyId:");
-                    car.CompanyId = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("CurrentKm:");
-                    car.CurrentKm = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("DailyKmLimit:");
-                    car.DailyKmLimit = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("DailyPrice:");
-                    car.DailyPrice = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("DriverLicenseAge:");
-                    car.DriverLicenseAge = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("MinAge:");
-                    car.MinAge = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("SeatCount:");
-                    car.SeatCount = Convert.ToInt32(Console.ReadLine());
-                    var url = await CreateCarAsync(car);
-                    Console.WriteLine($"Created at {url}");
-                }
-                else if(i=="2")
-                {
-                    RentedCar car = new RentedCar();
-                    Console.WriteLine("Car Id:");
-                    car.carId = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("CustomerName:");
-                    car.CustomerName = Console.ReadLine();
-                    Console.WriteLine("CustomerSurname:");
-                    car.CustomerSurname = Console.ReadLine();
-                    Console.WriteLine("CustomerTC:");
-                    car.CustomerTC = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("DeliveryDate:");
-                    car.DeliveryDate = Convert.ToDateTime(Console.ReadLine());
-                    Console.WriteLine("RentDate:");
-                    car.RentDate = Convert.ToDateTime(Console.ReadLine());
-                    var url = await CreateRentedCarAsync(car);
-                    Console.WriteLine($"Created at {url}");
-                }
+                //string i = "";
+                //Console.WriteLine("Araç güncellemek için (1)");
+                //i = Console.ReadLine();
+                //if(i=="1")
+                //{
+                //    //Car car = new Car();
+                //    //Console.Write("Airbag:");
+                //    //car.Airbag =Convert.ToBoolean(Console.ReadLine());
+                //    //Console.WriteLine("BaggageCapacity:");
+                //    //car.BaggageCapacity = Convert.ToInt32(Console.ReadLine());
+                //    //Console.WriteLine("CarModel:");
+                //    //car.CarModel = Console.ReadLine();
+                //    //Console.WriteLine("CarName:");
+                //    //car.CarName=Console.ReadLine();
+                //    //Console.WriteLine("CompanyId:");
+                //    //car.CompanyId = Convert.ToInt32(Console.ReadLine());
+                //    //Console.WriteLine("CurrentKm:");
+                //    //car.CurrentKm = Convert.ToInt32(Console.ReadLine());
+                //    //Console.WriteLine("DailyKmLimit:");
+                //    //car.DailyKmLimit = Convert.ToInt32(Console.ReadLine());
+                //    //Console.WriteLine("DailyPrice:");
+                //    //car.DailyPrice = Convert.ToInt32(Console.ReadLine());
+                //    //Console.WriteLine("DriverLicenseAge:");
+                //    //car.DriverLicenseAge = Convert.ToInt32(Console.ReadLine());
+                //    //Console.WriteLine("MinAge:");
+                //    //car.MinAge = Convert.ToInt32(Console.ReadLine());
+                //    //Console.WriteLine("SeatCount:");
+                //    //car.SeatCount = Convert.ToInt32(Console.ReadLine());
+                //    //var url = await CreateCarAsync(car);
+                //    //Console.WriteLine($"Created at {url}");
+                //}
+                //else if(i=="2")
+                //{
+                RentedCar car = new RentedCar();
+                Console.WriteLine("Car Id:");
+                car.carId = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("New Km:");
+                car.NewKm = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Price:");
+                car.Price = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Updating...");
+                await UpdateRentedCarAsync(car);
+                //}
             }
             catch(Exception e)
             {
